dcced3dabc208903ed05e825e0e666d97cbeb75b
JarrodKane <kajarrod@gmail.com>
2020-12-07 11:44:41 +1100

    Added in some more weather info and clean of app.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       callback(
         undefined,
-        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out.`
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress outside, it feels like ${body.current.feelslike}.`
       );
     }
   });
